af3b4f7efd0f217309e4551ad30b8f8e517efa6f
JafarAkhondali <jafar.akhoondali@gmail.com>
2024-02-22 13:34:12 +0100

    + Enable feature to notif debugging group on error

--- h2snotifier/main.py
+++ h2snotifier/main.py
@@ -9,7 +9,7 @@ import json
 
 env = dotenv_values(".env")
 TELEGRAM_API_KEY = env.get("TELEGRAM_API_KEY")
-TELEGRAM_API_KEY = env.get("TELEGRAM_API_KEY")
+DEBUGGING_CHAT_ID = env.get("DEBUGGING_CHAT_ID")
 
 
 def read_config(config_path="config.json"):
@@ -29,12 +29,18 @@ def main():
             new_houses = sync_houses(city_id=city_id, houses=houses)
 
             for h in new_houses:
-                if len(h['images']) > 0:
-                    telegram.send_media_group(h['images'][:4], house_to_msg(h))
-                else:
-                    telegram.send_simple_msg(house_to_msg(h))
-
-                logging.info(f"Sent telegram Notif for {h['url_key']}")
+                try:
+                    if len(h['images']) > 0:
+                        telegram.send_media_group(h['images'][:4], house_to_msg(h))
+                    else:
+                        telegram.send_simple_msg(house_to_msg(h))
+                    logging.info(f"Sent telegram Notif for {h['url_key']}")
+                except Exception as error:
+                    debug_telegram = TelegramBot(apikey=TELEGRAM_API_KEY, chat_id=DEBUGGING_CHAT_ID)
+                    debug_telegram.send_simple_msg(f"Sending notification failed! {h}")
+                    debug_telegram.send_simple_msg(str(error))
+                    logging.info(f"Sending notification failed! {h}")
+                    logging.info(str(error))
 
 
 if __name__ == "__main__":
